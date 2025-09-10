a9277851657596f698f94533618fe396d6249ddb
Sylvester Praise <sylvesterpraise123456@gmail.com>
2025-09-10 13:34:55 +0100

    updates

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+        
+def find_post_index(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i;
+               
 
 # class Post(BaseModel):
 #     title: str
@@ -51,24 +57,34 @@ def get_posts():
     return {"message": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def my_post(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(1,1000000)
     my_posts.append(post_dict)
     return {
-        "Data" : post_dict
+    "Data" : post_dict
     }
-    
+
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
+    if not post:
+         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     print(post)
     return {
         "post_details":  post
     }
       
-    
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_post_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id: {id} does not exist")
+    my_posts.pop(index)
+    return {
+        Response(status_code = status.HTTP_204_NO_CONTENT)
+    }
 
 # @app.post("/posts", status_code=status.HTTP_201_CREATED)
 # def create_post(post: Post):
