d94c87a9191f30e9eeb80817bb39a75eea6beca2
Sylvester Praise <sylvesterpraise123456@gmail.com>
2025-09-10 13:58:23 +0100

    updates

--- main.py
+++ main.py
@@ -85,6 +85,18 @@ def delete_post(id: int):
     return {
         Response(status_code = status.HTTP_204_NO_CONTENT)
     }
+    
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+    index = find_post_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {
+        "data": post_dict
+    }
 
 # @app.post("/posts", status_code=status.HTTP_201_CREATED)
 # def create_post(post: Post):
