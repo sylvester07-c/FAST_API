ae3fe962a1a406701610cf465c333d366ea864f0
Sylvester Praise <sylvesterpraise123456@gmail.com>
2025-09-11 19:35:22 +0100

    updates

--- app/main.py
+++ app/main.py
@@ -28,25 +28,6 @@ def find_post_index(id):
             return i;
                
 
-# class Post(BaseModel):
-#     title: str
-#     content: str
-#     rating: Optional[int] = None
-
-
-# my_posts = [{"title":"My Post 1", "content":"This is my post content 1", "id":1}, {"title":"My Post 2", "content":"This is my post content 2", "id":2}]
-
-# def find_post(id):
-#     for p in my_posts:
-#         if p['id'] == id:
-#             return p
-
-# def find_index_post(id):
-#     for i, p in enumerate(my_posts):
-#         if p['id'] == id:
-#             return i
-
-
 @app.get("/")
 async def root():
     return {"Hello": "mum"}
@@ -98,37 +79,4 @@ def update_post(id:int, post:Post):
         "data": post_dict
     }
 
-# @app.post("/posts", status_code=status.HTTP_201_CREATED)
-# def create_post(post: Post):
-#     post_dict = post.dict() # Convert Pydantic model to dictionary
-#     post_dict["id"] = randint(0, 1000000000000)  # Assign a random ID for the new post
-#     my_posts.append(post_dict)
-#     return {"data": post_dict}
-
-# @app.get("/posts/latest")
-# def get_latest_post():
-#     post = my_posts[len(my_posts)-1]
-#     return {"detail": post}
-
-# @app.get("/posts/{id}")
-# def get_post(id: int):
-#     post = find_post(id)
-#     if not post:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-#                             detail=f"post with id: {id} was not found")
-
-#     return {"post_detail": post}
-
-# @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-# def delete_post(id: int):
-#     index = find_index_post(id)
-
-#     if index == None:
-#         raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail=f"post with id: {id} does not exist")
-#     my_posts.pop(index)
-#     return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-# @app.put("/posts/{id}")
-# def update_post(id: int, post:Post):
-#     print(post)
-#     return{'message': "update post"}
+ 
